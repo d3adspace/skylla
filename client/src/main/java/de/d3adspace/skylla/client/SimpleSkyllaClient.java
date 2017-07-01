cc50850122f68a447d5aa6b467eb3f824a75270a
Nathalie0hneHerz <nathalie@d3adspace.de>
2017-07-01 22:41:03 +0200

    fill client documentation

--- client/src/main/java/de/d3adspace/skylla/client/SimpleSkyllaClient.java
+++ client/src/main/java/de/d3adspace/skylla/client/SimpleSkyllaClient.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2017 D3adspace
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package de.d3adspace.skylla.client;
 
 import de.d3adspace.skylla.commons.config.SkyllaConfig;
@@ -10,14 +31,32 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.EventLoopGroup;
 
 /**
+ * Basic client.
+ *
  * @author Nathalie0hneHerz
  */
 public class SimpleSkyllaClient implements SkyllaClient {
 	
+	/**
+	 * Config providing server adress
+	 */
 	private final SkyllaConfig config;
+	
+	/**
+	 * Worker group for netty.
+	 */
 	private EventLoopGroup workerGroup;
+	
+	/**
+	 * Channel to communicate with the server
+	 */
 	private Channel channel;
 	
+	/**
+	 * Create a new client
+	 *
+	 * @param config The config.
+	 */
 	SimpleSkyllaClient(SkyllaConfig config) {
 		this.config = config;
 	}
