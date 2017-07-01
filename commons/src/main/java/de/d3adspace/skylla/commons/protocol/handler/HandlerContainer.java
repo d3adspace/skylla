10c6be6ab05cade51f4b9f22fc37490ad5e2a3c6
Nathalie0hneHerz <nathalie@d3adspace.de>
2017-07-01 14:01:28 +0200

    add method parameter to be able the get the source connection of a packet

--- commons/src/main/java/de/d3adspace/skylla/commons/protocol/handler/HandlerContainer.java
+++ commons/src/main/java/de/d3adspace/skylla/commons/protocol/handler/HandlerContainer.java
@@ -1,5 +1,27 @@
+/*
+ * Copyright (c) 2017 D3adspace
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package de.d3adspace.skylla.commons.protocol.handler;
 
+import de.d3adspace.skylla.commons.connection.SkyllaConnection;
 import de.d3adspace.skylla.commons.protocol.packet.SkyllaPacket;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -31,10 +53,10 @@ public class HandlerContainer {
 		this.registeredListeners.remove(packetHandler);
 	}
 	
-	public void handlePacket(SkyllaPacket packet) {
+	public void handlePacket(SkyllaConnection skyllaConnection, SkyllaPacket packet) {
 		this.registeredListeners.forEach((handler, methods) -> methods.forEach(method -> {
 			try {
-				method.invoke(handler, packet);
+				method.invoke(handler, skyllaConnection, packet);
 			} catch (IllegalAccessException | InvocationTargetException e) {
 				e.printStackTrace();
 			}
--- example/src/main/java/de/d3adspace/example/SkyllyClientExample.java
+++ example/src/main/java/de/d3adspace/example/SkyllyClientExample.java
@@ -1,10 +1,31 @@
+/*
+ * Copyright (c) 2017 D3adspace
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package de.d3adspace.example;
 
-import de.d3adspace.skylla.client.SimpleSkyllaClient;
 import de.d3adspace.skylla.client.SkyllaClient;
 import de.d3adspace.skylla.client.SkyllaClientFactory;
 import de.d3adspace.skylla.commons.config.SkyllaConfig;
 import de.d3adspace.skylla.commons.config.SkyllaConfigBuilder;
+import de.d3adspace.skylla.commons.connection.SkyllaConnection;
 import de.d3adspace.skylla.commons.protocol.Protocol;
 import de.d3adspace.skylla.commons.protocol.handler.PacketHandler;
 import de.d3adspace.skylla.commons.protocol.handler.PacketHandlerMethod;
@@ -34,7 +55,7 @@ public class SkyllyClientExample {
 	public static class ClientPacketHandlerExample implements PacketHandler {
 		
 		@PacketHandlerMethod
-		public void onPacketChat(ChatPacket chatPacket) {
+		public void onPacketChat(SkyllaConnection skyllaConnection, ChatPacket chatPacket) {
 			System.out.println("[Client] received: " + chatPacket);
 		}
 	}
